c240db4543b73adcf406d528e0f50160b1c53b82
ngcsontrh <trhsonngoc12@gmail.com>
2025-11-15 01:47:33 +0700

    Init project

---
+++ src/BSM.Domain/Identity/AppUserRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BSM.Domain.Identity;
+
+public class AppUserRole : IdentityUserRole<Guid>
+{
+    
+}
---
+++ src/BSM.Infrastructure/Persistent/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BSM.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BSM.Infrastructure.Persistent;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : IdentityDbContext<AppUser, AppRole, Guid, AppUserClaim, AppUserRole, AppUserLogin, AppRoleClaim, AppUserToken,
+        AppUserPasskey>(options)
+{
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
